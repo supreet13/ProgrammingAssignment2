866e583c5bd17dd096b4c21691d5a6e33d8bba80
Supreet Singh Jossan <supreet.jossan@gmail.com>
2014-08-25 00:45:31 +0530

    functions for inverse matrix
    code and comments return

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates a matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())
+  {
+                                         m <- NULL
+                                           set <- function(y) {
+                                             x <<- y
+                                             m <<- NULL
+                                           }
+                                           get <- function() x
+                                           setsolve <- function(solve) m <<- solve
+                                           getsolve <- function() m
+                                           list(set = set, get = get,
+                                                setsolve = setsolve,
+                                                getsolve = getsolve)
+                                           
 
 }
 
 
-## Write a short comment describing this function
+## This function returns inverse of Matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+makeVector <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
